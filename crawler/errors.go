d05da13d8ade2546954fccb900142b608f1b9c16
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-31 16:33:59 -0300

    Add crawler errors definition

---
+++ crawler/errors.go
@@ -0,0 +1,9 @@
+package crawler
+
+import "errors"
+
+// Error for plan not found
+var errPlanIndexNotFound = errors.New("Plan index not found")
+
+// Error for fee not found
+var errPlanFeeNotFound = errors.New("Fee fee not found")
