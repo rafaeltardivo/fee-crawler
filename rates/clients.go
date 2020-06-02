53fa51b9a0cc958e6daec2630db892379f445856
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-02 17:47:41 -0300

    Add exchange rates client implementation

---
+++ rates/clients.go
@@ -0,0 +1,49 @@
+package rates
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// Interface for rate clients
+type clientInterface interface {
+	GetRates() ([]byte, error)
+}
+
+type exchangeRatesClient struct {
+	url     string
+	filters string
+}
+
+// Requests currency data from REST api and returns byte array containig the raw response
+func (c *exchangeRatesClient) GetRates() ([]byte, error) {
+	var endpoint bytes.Buffer
+	endpoint.WriteString(c.url)
+	endpoint.WriteString(c.filters)
+	request, err := http.Get(endpoint.String())
+	if err != nil {
+		return nil, err
+	}
+	defer request.Body.Close()
+
+	if request.StatusCode != http.StatusOK {
+		return nil, clientError(fmt.Sprintf("HTTP Status code: %d", request.StatusCode))
+	}
+
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+// Creates and returns a new exchange rates client according to clientInterface
+func NewExchangeRatesClient() clientInterface {
+	return &exchangeRatesClient{
+		url:     "https://api.exchangeratesapi.io/latest",
+		filters: "?base=BRL&symbols=USD,EUR",
+	}
+}
