7d246aa6617d3e0c17e9e98ed193cd590bcf6428
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-07 12:00:57 -0300

    Update clients structure

--- rates/clients.go
+++ rates/clients.go
@@ -2,49 +2,154 @@ package rates
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+
+	"github.com/go-redis/redis"
 )
 
-// Interface for rate clients
-type clientInterface interface {
-	GetRates() ([]byte, error)
+const KEY string = "rates"
+
+// Database connection data definition
+type databaseConnectionData struct {
+	host     string
+	port     string
+	password string
 }
 
-// ExchangeRates client implementation of clientInterface
-type exchangeRatesClient struct {
-	url     string
-	filters string
+// Interface for database operations
+type databaseInterface interface {
+	getConnectionData() *databaseConnectionData
+	cacheRates(*exchangeRateResponsePayload) error
+	fetchCachedRates() ([]byte, error)
 }
 
-// Requests currency data from REST api and returns byte array containig the raw response
-func (c *exchangeRatesClient) GetRates() ([]byte, error) {
-	var endpoint bytes.Buffer
-	endpoint.WriteString(c.url)
-	endpoint.WriteString(c.filters)
-	request, err := http.Get(endpoint.String())
+// Endpoint (url + querystring) data
+type endpointData struct {
+	url         string
+	queryString string
+}
+
+// Interface for web client operations
+type webClientInterface interface {
+	getEndpointData() *endpointData
+	fetchLatestRates() ([]byte, error)
+}
+
+// Redis implementation of databaseInterface
+type redisDatabase struct{}
+
+// Exchange rates implementation of webClientInterface
+type exchangeRatesAPI struct{}
+
+// Returns database connection data
+func (db *redisDatabase) getConnectionData() *databaseConnectionData {
+	return &databaseConnectionData{
+		host:     os.Getenv("DB_HOST"),
+		port:     os.Getenv("DB_PORT"),
+		password: os.Getenv("DB_PASSWORD"),
+	}
+}
+
+// Returns a new database client
+func (db *redisDatabase) getConnection() (*redis.Client, error) {
+	var connectionString bytes.Buffer
+	config := db.getConnectionData()
+
+	connectionString.WriteString(config.host)
+	connectionString.WriteString(":")
+	connectionString.WriteString(config.port)
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     connectionString.String(),
+		Password: config.password,
+		DB:       0,
+	})
+
+	_, err := client.Ping().Result()
 	if err != nil {
+		logger.Error(err)
 		return nil, err
+	}
+	return client, nil
+}
+
+// Returns cached rates payload
+func (db *redisDatabase) fetchCachedRates() ([]byte, error) {
+	client, err := db.getConnection()
+	if err != nil {
+		return nil, databaseError("could not connect to database")
+	}
+	defer client.Close()
+
+	logger.Info("querying cache for rates")
+	rates, err := client.Get(KEY).Bytes()
+	if err != nil {
+		logger.Error(err)
+		return nil, databaseError(fmt.Sprintf("key not found: %s", KEY))
+	}
+
+	logger.Info("retrieved rates from cache")
+	return rates, nil
+}
+
+// Caches rates payload
+func (db *redisDatabase) cacheRates(payload *exchangeRateResponsePayload) error {
+	client, err := db.getConnection()
+	if err != nil {
+		return databaseError("could not connect to database")
+	}
+	defer client.Close()
+
+	data, _ := json.Marshal(payload)
+	logger.Info(fmt.Sprintf("setting cache for %s", KEY))
+	client.Set(KEY, data, 0)
+	return nil
+}
+
+// Returns external rates api endpoint configuration
+func (api *exchangeRatesAPI) getEndpointData() *endpointData {
+	return &endpointData{
+		url:         os.Getenv("EXCHANGE_RATES_URL"),
+		queryString: os.Getenv("EXCHANGE_RATES_QUERYSTRING"),
+	}
+}
+
+// Returns latest rates from exchange rates api
+func (api *exchangeRatesAPI) fetchLatestRates() ([]byte, error) {
+	config := api.getEndpointData()
+
+	logger.Trace("requesting API for latest rates")
+	request, err := http.Get(config.url + config.queryString)
+	if err != nil {
+		logger.Error(err)
+		return nil, apiError("API request failed")
 	}
 	defer request.Body.Close()
 
 	if request.StatusCode != http.StatusOK {
-		return nil, clientError(fmt.Sprintf("HTTP Status code: %d", request.StatusCode))
+		return nil, apiError(fmt.Sprintf("HTTP Status code - %d", request.StatusCode))
 	}
 
 	body, err := ioutil.ReadAll(request.Body)
 	if err != nil {
-		return nil, err
+		logger.Error(err)
+		return nil, apiError("could not parse body")
 	}
 
+	logger.Info("retrieved latest rates from api")
 	return body, nil
 }
 
-// Creates and returns a new exchange rates client according to clientInterface
-func NewExchangeRatesClient() clientInterface {
-	return &exchangeRatesClient{
-		url:     "https://api.exchangeratesapi.io/latest",
-		filters: "?base=BRL&symbols=USD,EUR",
-	}
+// Returns a new redis database client according to databaseInterface
+func newRedisDatabaseClient() databaseInterface {
+	return &redisDatabase{}
+}
+
+// Returns a new exchange rates api client according to webClientInterface
+func newExchangeRatesAPIClient() webClientInterface {
+	return &exchangeRatesAPI{}
 }
--- rates/clients_test.go
+++ rates/clients_test.go
@@ -1,16 +1,47 @@
 package rates
 
 import (
+	"os"
 	"testing"
 
 	"github.com/onsi/gomega"
 )
 
-func TestNewExchangeRatesClientType(t *testing.T) {
+func TestNewRedisDatabaseClient(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 
-	client := NewExchangeRatesClient()
-	_, sameType := client.(clientInterface)
+	client := newRedisDatabaseClient()
+	_, isOne := client.(databaseInterface)
 
-	g.Expect(sameType).To(gomega.BeTrue(), "client is-one clientInterface")
+	g.Expect(isOne).To(gomega.BeTrue(), "redis client is-one databaseInterface")
+}
+
+func TestNewExchangeRatesAPIClient(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	client := newExchangeRatesAPIClient()
+	_, isOne := client.(webClientInterface)
+
+	g.Expect(isOne).To(gomega.BeTrue(), "exchange rates client is-one webClientInterface")
+}
+
+func TestRedisDatabaseClientConnectionData(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	client := newRedisDatabaseClient()
+	configurationData := client.getConnectionData()
+
+	g.Expect(configurationData.host).To(gomega.Equal(os.Getenv("DB_HOST")), "configuration host is equal to env var DB_HOST")
+	g.Expect(configurationData.port).To(gomega.Equal(os.Getenv("DB_PORT")), "configuration host is equal to env var DB_PORT")
+	g.Expect(configurationData.password).To(gomega.Equal(os.Getenv("DB_PASSWORD")), "configuration host is equal to env var PASSWORD")
+}
+
+func TestExchangeRatesClientEndpointData(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	client := newExchangeRatesAPIClient()
+	endpointData := client.getEndpointData()
+
+	g.Expect(endpointData.url).To(gomega.Equal(os.Getenv("EXCHANGE_RATES_URL")), "url is equal to env var EXCHANGE_RATES_URL")
+	g.Expect(endpointData.queryString).To(gomega.Equal(os.Getenv("EXCHANGE_RATES_QUERYSTRING")), "queryString is equal to env var EXCHANGE_RATES_QUERYSTRING")
 }
