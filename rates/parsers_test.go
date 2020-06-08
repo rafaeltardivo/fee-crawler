afb346108198fe568f0c651f8a09512afa2beebf
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-08 00:02:00 -0300

    Update parsers tests

--- rates/parsers_test.go
+++ rates/parsers_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/onsi/gomega"
 )
 
-func TestnewExchangeRatesParserType(t *testing.T) {
+func TestNewExchangeRatesParserType(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 
 	parser := newExchangeRatesParser()
@@ -19,7 +19,7 @@ func TestnewExchangeRatesParserType(t *testing.T) {
 
 func TestParserRatesRatesError(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
-	exchangeRateResponseMock := exchangeRateResponsePayload{
+	exchangeRateResponseMock := ExchangeRatesResponsePayload{
 		Rates: exchangeRateCurrencyPayload{
 			EUR: 0.00,
 			USD: 0.00,
@@ -33,14 +33,14 @@ func TestParserRatesRatesError(t *testing.T) {
 	parser := newExchangeRatesParser()
 	ret, err := parser.ParseRates(rawBody.Bytes())
 
-	g.Expect(ret).Should(gomega.BeNil(), "Return should be nil")
-	g.Expect(err).Should(gomega.HaveOccurred(), "An error should have occurred")
-	g.Expect(err).Should(gomega.MatchError(parseError("invalid rate data")), "Error should be a parseError")
+	g.Expect(ret).To(gomega.BeNil(), "Return should be nil")
+	g.Expect(err).To(gomega.HaveOccurred(), "An error should have occurred")
+	g.Expect(err).To(gomega.MatchError(parseError("invalid rate data")), "Error should be a parseError")
 }
 
 func TestParserRatesDateError(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
-	exchangeRateResponseMock := exchangeRateResponsePayload{
+	exchangeRateResponseMock := ExchangeRatesResponsePayload{
 		Rates: exchangeRateCurrencyPayload{
 			EUR: 0.1687023416,
 			USD: 0.1875295229,
@@ -54,14 +54,14 @@ func TestParserRatesDateError(t *testing.T) {
 	parser := newExchangeRatesParser()
 	ret, err := parser.ParseRates(rawBody.Bytes())
 
-	g.Expect(ret).Should(gomega.BeNil(), "Return should be nil")
-	g.Expect(err).Should(gomega.HaveOccurred(), "An error should have occurred")
-	g.Expect(err).Should(gomega.MatchError(parseError("invalid date format")), "Error should be a parseError")
+	g.Expect(ret).To(gomega.BeNil(), "Return should be nil")
+	g.Expect(err).To(gomega.HaveOccurred(), "An error should have occurred")
+	g.Expect(err).To(gomega.MatchError(parseError("invalid date format")), "Error should be a parseError")
 }
 
 func TestParserRatesBaseError(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
-	exchangeRateResponseMock := exchangeRateResponsePayload{
+	exchangeRateResponseMock := ExchangeRatesResponsePayload{
 		Rates: exchangeRateCurrencyPayload{
 			EUR: 0.1687023416,
 			USD: 0.1875295229,
@@ -75,14 +75,14 @@ func TestParserRatesBaseError(t *testing.T) {
 	parser := newExchangeRatesParser()
 	ret, err := parser.ParseRates(rawBody.Bytes())
 
-	g.Expect(ret).Should(gomega.BeNil(), "Return should be nil")
-	g.Expect(err).Should(gomega.HaveOccurred(), "An error should have occurred")
-	g.Expect(err).Should(gomega.MatchError(parseError("invalid base rate")), "Error should be a parseError")
+	g.Expect(ret).To(gomega.BeNil(), "Return should be nil")
+	g.Expect(err).To(gomega.HaveOccurred(), "An error should have occurred")
+	g.Expect(err).To(gomega.MatchError(parseError("invalid base rate")), "Error should be a parseError")
 }
 
 func TestParserRates(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
-	exchangeRateResponseMock := exchangeRateResponsePayload{
+	exchangeRateResponseMock := ExchangeRatesResponsePayload{
 		Rates: exchangeRateCurrencyPayload{
 			EUR: 0.1687023416,
 			USD: 0.1875295229,
@@ -95,8 +95,8 @@ func TestParserRates(t *testing.T) {
 
 	parser := newExchangeRatesParser()
 	ret, err := parser.ParseRates(rawBody.Bytes())
-	exchangeRateData, _ := ret.(*exchangeRateResponsePayload)
+	exchangeRateData, _ := ret.(*ExchangeRatesResponsePayload)
 
-	g.Expect(err).ShouldNot(gomega.HaveOccurred(), "An error should not have occurred")
-	g.Expect(ret).Should(gomega.Equal(exchangeRateData), "Return should be equal to exchangeRateData")
+	g.Expect(err).ToNot(gomega.HaveOccurred(), "An error should not have occurred")
+	g.Expect(ret).To(gomega.Equal(exchangeRateData), "Return should be equal to exchangeRateData")
 }
